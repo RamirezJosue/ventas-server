2c407e8cddd01c54c52d74a197ca858bd03e8a20
RamirezJosue <xdemobtkx@gmail.com>
2018-04-24 19:00:21 -0500

    Actualizar Usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -31,6 +31,70 @@ app.get('/', (req, res, next) => {
             });
 });
 
+// ==========================================
+// Actualizar usuario
+// ==========================================
+
+app.put('/:id', (req, res) => {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    Usuario.findById(id, (err, usuario) => {
+
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con el id ' + id + ' no existe',
+                errors: { message: 'No existe un usuario con ese ID' }
+            });
+        }
+
+
+        usuario.nombre = body.nombre;
+        usuario.tipoDocumento = body.tipoDocumento;
+        usuario.numDocumento = body.numDocumento;
+        usuario.direccion = body.direccion;
+        usuario.telefono = body.telefono;
+        usuario.email = body.email;
+        usuario.login = body.login;
+        usuario.role = body.role;
+
+
+
+        usuario.save((err, usuarioGuardado) => {
+
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar usuario',
+                    errors: err
+                });
+            }
+
+            usuarioGuardado.clave = ':)';
+
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado
+            });
+
+        });
+
+    });
+
+});
+
+
 
 // ==========================================
 // Crear un nuevo usuario
